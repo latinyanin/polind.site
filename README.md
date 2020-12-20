b2653bf92c52b2ef09ba76d210c5225d7efa5a11
TaRo <latinyanin@gmail.com>
2020-12-20 14:58:24 +0200

    Added wedpack

--- README.md
+++
@@ -1,1 +0,0 @@
-# polind.site
---
+++ src/js/app.js
@@ -0,0 +1,10 @@
+/* jshint ignore:start */
+import '../../node_modules/focus-visible/dist/focus-visible';
+import $ from 'jquery';
+import 'popper.js';
+import 'bootstrap';
+import '../scss/style.scss';
+import '../index.html'; // jshint ignore:line
+import './script.js';
+
+/* jshint ignore:end */
